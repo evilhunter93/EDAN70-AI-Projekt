ee40f13794c253452b8ddc49de4dea49a9139e01
ludd-e <ludd-e@users.noreply.github.com>
2017-03-29 17:56:35 +0200

    Added Colour to Player

--- Tak/Game/Player.cs
+++ Tak/Game/Player.cs
@@ -4,12 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+enum Colour
+{
+    Black, White
+}
+
 namespace Tak.Game
 {
     abstract class Player
     {
         int nbrCapStones;
         int nbrFlatStones;
+        private Colour colour;
 
         public virtual void doMove()
         {
