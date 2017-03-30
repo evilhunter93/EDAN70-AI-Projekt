fd9e4ea19c0b9e247f5527cb3aff194ee8f9729b
Flyttkartong <johansson.samuel@live.se>
2017-03-30 14:32:33 +0200

    Started work on GameBoard, added Count to StoneStack, created IllegalMoveException

--- Tak/Game/GameBoard.cs
+++ Tak/Game/GameBoard.cs
@@ -8,5 +8,27 @@ namespace Tak.Game
 {
     class GameBoard
     {
+        const int UNSPECIFIED = -1;
+        private int size;
+        private StoneStack[,] stacks;
+
+        public GameBoard(int size)
+        {
+            this.size = size;
+            stacks = new StoneStack[size, size];
+        }
+
+        public bool PlaceStone(int x, int y, Stone stone)
+        {
+            return false;
+        }
+
+        public bool PickUpStack(int x, int y, int amount = UNSPECIFIED)
+        {
+            if (amount == UNSPECIFIED)
+                if (stacks[x, y].Count > 1)
+                    throw new IllegalMoveException("Unspecified number of stones but stack has more than one");
+            return false;
+        }
     }
 }
