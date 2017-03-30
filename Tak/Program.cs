8251fbe6a5cb47e02e236c425b5295f55d2af848
Flyttkartong <johansson.samuel@live.se>
2017-03-30 19:00:30 +0200

    Program no longer calls tests

--- Tak/Program.cs
+++ Tak/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tak.Tests;
 
 namespace Tak
 {
@@ -11,14 +10,6 @@ namespace Tak
     {
         static void Main(string[] args)
         {
-            TestClone test = new TestClone();
-
-            test.Test0();
-
-            test.Test1();
-
-            test.Test2();
-
             Console.Write("\nPress any key to exit...");
             Console.ReadLine();
         }
