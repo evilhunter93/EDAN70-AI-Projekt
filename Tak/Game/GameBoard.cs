98b2ebfdf88d4a8f767c45f537e2ed6e3430855e
ludd-e <ludd-e@users.noreply.github.com>
2017-04-05 16:07:00 +0200

    Added GameState enum. Added CheckOwner. Added game end check (UpdateGameState()), for "Flat victory".

--- Tak/Game/GameBoard.cs
+++ Tak/Game/GameBoard.cs
@@ -5,6 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 using Tak.Utilities;
 
+public enum GameState
+{
+    WR, BR, WF, BF, Tie, InProgress
+}
+
 namespace Tak.Game
 {
     public class GameBoard
@@ -12,6 +17,11 @@ namespace Tak.Game
         public const int UNSPECIFIED = -1;
         private int size;
         private StoneStack[,] stacks;
+        private Colour turn;
+        private GameState state;
+
+        public Colour Turn { get { return turn; } set { turn = value; } }
+        public GameState GameState { get { return state; } }
 
         public StoneStack[,] Stacks
         {
@@ -32,6 +42,7 @@ namespace Tak.Game
 
         public GameBoard(int size)
         {
+            turn = Colour.Black;
             this.size = size;
             stacks = new StoneStack[size, size];
             for (int x = 0; x < size; x++)
@@ -53,6 +64,7 @@ namespace Tak.Game
         public StoneStack PickUpStack(int x, int y, int amount = UNSPECIFIED)
         {
             CheckIndex(x, y);
+            CheckOwner(x, y);
 
             if (amount == UNSPECIFIED)
             {
@@ -67,6 +79,69 @@ namespace Tak.Game
 
             StoneStack pickedUp = stacks[x, y].Separate(amount);
             return pickedUp;
+        }
+
+        public Colour EndTurn()
+        {
+            UpdateGameState();
+            turn = (turn == Colour.Black) ? Colour.White : Colour.Black;
+            return turn;
+        }
+
+        private void UpdateGameState()
+        {
+            if (FullBoard())
+            {
+                int nbrWhite = 0, nbrBlack = 0;
+                for (int i = 0; i < size; i++)
+                    for (int j = 0; j < size; j++)
+                    {
+                        if (stacks[i, j].Owner == Colour.Black)
+                            nbrBlack++;
+                        else
+                            nbrWhite++;
+                    }
+
+                if (nbrWhite > nbrBlack)
+                    state = GameState.WF;
+                else if (nbrWhite < nbrBlack)
+                    state = GameState.BF;
+                else
+                    state = GameState.Tie;
+            }
+            else if (RoadExists())
+            {
+                // TODO
+            }
+            else
+            {
+                state = GameState.InProgress;
+            }
+
+
+
+        }
+
+        private bool RoadExists()
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+
+        private bool FullBoard()
+        {
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                    if (stacks[i, j].Count == 0)
+                        return false;
+
+            return true;
+        }
+
+        private void CheckOwner(int x, int y)
+        {
+            if (stacks[x, y].Owner != turn)
+                throw new IllegalMoveException("\nCurrent player does not control the stack at [" + x + ", " + y + "].");
         }
 
         private void CheckIndex(int x, int y)
