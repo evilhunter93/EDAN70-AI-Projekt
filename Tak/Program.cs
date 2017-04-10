52d28f072e5d5b86eb7590a9359f572c8eccaab5
Flyttkartong <johansson.samuel@live.se>
2017-04-10 17:53:03 +0200

    Implemented Program.

--- Tak/Program.cs
+++ Tak/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tak.Game;
 
 namespace Tak
 {
@@ -10,6 +11,9 @@ namespace Tak
     {
         static void Main(string[] args)
         {
+            TakGame game = new TakGame(4, "Human", "Human");
+            game.Start();
+
             Console.Write("\nPress any key to exit...");
             Console.ReadLine();
         }
