11006c126d99ef0733e37175f12f1ff684771e83
Kevin <KOOL_music@hotmail.com>
2017-05-05 15:28:50 +0200

    Enkel build på hur minMax är tänkt att fungera

--- Tak/AI/MiniMaxAI.cs
+++ Tak/AI/MiniMaxAI.cs
@@ -9,19 +9,111 @@ namespace Tak.AI
 {
     class MiniMaxAI : AI
     {
+        private GameBoard board;
+        public MiniMaxAI(GameBoard board)
+        {
+            this.board = board;
+        }
         public string BestMove()
         {
+            String move = MinMax();
             throw new NotImplementedException();
         }
 
-        public void MiniMax()
+        private String MinMax()
         {
+            String bestMove = null;
+            String[] moves;
+            System.Collections.ArrayList nodes;
+
             // Find all valid moves
+            moves = board.validMoves();
+            nodes = new System.Collections.ArrayList();
 
             // For each valid move
             // Create a node
+            foreach (String move in moves)
+            {
+                nodes.Add(new Node(board, move));
+            }
 
             // Recursively find the best move by using the minimax algorithm (iterative deepening) on the nodes
+            Node node;
+            int temp;
+            int min = int.MaxValue;
+            while (nodes.Count > 0)
+            {
+                node = (Node)nodes[0];
+                nodes.Remove(node);
+                temp = min(node);
+                if (temp < min)
+                {
+                    bestMove = node.move;
+                }
+            }
+            return bestMove;
+        }
+
+        private int Min(Node node)
+        {
+            String[] moves;
+            System.Collections.ArrayList nodes;
+
+            // Find all valid moves
+            moves = board.validMoves();
+            nodes = new System.Collections.ArrayList();
+
+            // For each valid move
+            // Create a node
+            foreach (String move in moves)
+            {
+                nodes.Add(new Node(board, move));
+            }
+
+            // Recursively find the best move by using the minimax algorithm (iterative deepening) on the nodes
+            Node newNode;
+            int temp;
+            int min = int.MaxValue;
+            while (nodes.Count > 0)
+            {
+                newNode = (Node)nodes[0];
+                nodes.Remove(newNode);
+                temp = Max(newNode);
+                if (temp < min)
+                    min = temp;
+            }
+            return min;
+        }
+
+        private int Max(Node node)
+        {
+            String[] moves;
+            System.Collections.ArrayList nodes;
+
+            // Find all valid moves
+            moves = board.validMoves();
+            nodes = new System.Collections.ArrayList();
+
+            // For each valid move
+            // Create a node
+            foreach (String move in moves)
+            {
+                nodes.Add(new Node(board, move));
+            }
+
+            // Recursively find the best move by using the minimax algorithm (iterative deepening) on the nodes
+            Node newNode;
+            int temp;
+            int max = int.MinValue;
+            while (nodes.Count > 0)
+            {
+                newNode = (Node)nodes[0];
+                nodes.Remove(newNode);
+                temp = Min(newNode);
+                if (temp > max)
+                    max = temp;
+            }
+            return max;
         }
 
         private class Node
@@ -31,11 +123,13 @@ namespace Tak.AI
             bool end;
             string move;
 
-            Node(GameBoard board, string move)
+            internal Node(GameBoard board, string move)
             {
                 this.board = board;
                 this.move = move;
             }
+
+
         }
     }
 }
