4939e2b290198b3bfec89cedec3db4c42089d4ce
Flyttkartong <johansson.samuel@live.se>
2017-05-09 16:11:12 +0200

    Extracted the evaluation function from MiniMaxAI to its own class.

---
+++ Tak/AI/Evaluator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tak.Game;
+
+namespace Tak.AI
+{
+    class Evaluator
+    {
+        public static int EvaluateStack(GameBoard board, Colour turn)
+        {
+            int score = 0;
+            StoneStack[,] stacks = board.StacksReference;
+            foreach (StoneStack stack in stacks)
+            {
+                if (stack.Count > 0)
+                {
+                    if (stack.Owner == turn)
+                    {
+                        score += stack.Count;
+                    }
+                    else
+                    {
+                        score -= stack.Count;
+                    }
+                }
+            }
+            return score;
+        }
+
+        public static int EvaluateTopPiece(GameBoard board, Colour turn)
+        {
+            int score = 0;
+            StoneStack[,] stacks = board.StacksReference;
+            foreach (StoneStack stack in stacks)
+            {
+                if (stack.Count > 0)
+                {
+                    if (stack.Owner == turn)
+                    {
+                        score += 1;
+                    }
+                    else
+                    {
+                        score -= 1;
+                    }
+                }
+            }
+            return score;
+        }
+    }
+}
