35811308702677144faa38769b1663b3454b9633
Flyttkartong <johansson.samuel@live.se>
2017-06-28 22:01:48 +0200

    Started work on ProximityScore.

--- Tak/AI/Evaluator.cs
+++ Tak/AI/Evaluator.cs
@@ -51,6 +51,30 @@ namespace Tak.AI
             return board.LargestConnectedComponent(player);
         }
 
+        public static int ProximityScore(GameBoard board, Colour player)
+        {
+            // FIXME: score should benefit short distances and many pieces
+            int size = board.Size;
+            StoneStack stack;
+            int score = 0;
+            StoneStack[,] stacks = board.StacksReference;
+            bool[,] visited = new bool[size, size];
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                {
+                    stack = stacks[i, j];
+                    if (stack.Count > 0)
+                        if (stack.Owner == player)
+                            if (stack.Top.Road)
+                            {
+                                visited = new bool[size, size];
+                                score += DistanceToNearest(i, j, stacks, size, visited, player);
+                            }
+                }
+
+            return -score;
+        }
+
         public static int GameStateScore(GameState gs, Colour player)
         {
             if (gs != GameState.InProgress && gs != GameState.Tie)
@@ -62,6 +86,30 @@ namespace Tak.AI
                     return int.MinValue;
             }
             return 0;
+        }
+
+        private static int DistanceToNearest(int i, int j, StoneStack[,] stacks, int size, bool[,] visited, Colour player)
+        {
+            if (i < 0 || i >= size || j < 0 || j >= size)
+                return int.MaxValue;
+
+            if (visited[i, j])
+                return int.MaxValue;
+
+            visited[i, j] = true;
+
+            if (stacks[i, j].Count > 0)
+                if (stacks[i, j].Owner == player)
+                    if (stacks[i, j].Top.Road)
+                        return 1;
+
+            int[] distances = new int[4];
+            distances[0] = DistanceToNearest(i - 1, j, stacks, size, visited, player);
+            distances[1] = DistanceToNearest(i + 1, j, stacks, size, visited, player);
+            distances[2] = DistanceToNearest(i, j - 1, stacks, size, visited, player);
+            distances[3] = DistanceToNearest(i, j + 1, stacks, size, visited, player);
+
+            return 1 + distances.Min();
         }
     }
 }
